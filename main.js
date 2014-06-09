dd5704f5bb770a2ec5933d6c141513fc99022e20
Irena Romendik <kidnemor@gmail.com>
2014-06-09 18:49:00 -0400

    oops

---
+++ main.js
@@ -0,0 +1,26 @@
+// comment
+var myNubmer = 3;
+var myString = "Hello!";
+var myBoolean = true;
+var myObject = {
+    numberOfFolds: 0,
+    title: "Origami",
+    myColor: function() {
+        return "rgb(255,0,0)";
+    }
+};
+var myArray = new Array(5);
+var myUnpredictableArray = [];
+var myPredictableArray = ["dog", "cat", "dinosaur"];
+
+function myFunction(a, b) {
+    return a + b;
+};
+console.log(myPredictableArray[0]);
+for (var i = 0, j = 28; i < 10, j > 0; i += 2, j--) {
+    document.write("<p>" + myFunction(i, j) + "</p>");
+}
+for (var p in myObject) {
+    console.log(myObject[p]);
+}
+document.write(myFunction(2, 3));
