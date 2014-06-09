b00aca795849a91840a1d6f7f3b541747be3bc03
Irena Romendik <kidnemor@gmail.com>
2014-06-09 18:51:20 -0400

    added comment

--- main.js
+++ main.js
@@ -16,7 +16,12 @@ var myPredictableArray = ["dog", "cat", "dinosaur"];
 function myFunction(a, b) {
     return a + b;
 };
+
 console.log(myPredictableArray[0]);
+
+/*
+Place breakpoint to line 25 and follow the way JS creates new paragraphs in the DOM
+*/
 for (var i = 0, j = 28; i < 10, j > 0; i += 2, j--) {
     document.write("<p>" + myFunction(i, j) + "</p>");
 }
